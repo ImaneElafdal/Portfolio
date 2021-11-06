6509b0d82928ea3a0e8782e479526dc7fd10cae8
ImaneElafdal <imanel2013@hotmail.fr>
2021-11-06 23:13:57 +0000

    init base

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>this is a FOOTER
+    </h1>
+</template>
+
+<script>
+
+export default {
+    name:"Footer"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="myname">
+   <h1 class="myname-title">Imane ElAfdal</h1>
+</div>
+</template>
+
+<script> 
+
+export default {
+  name: 'Home',
+  components: {
+     
+  }
+}
+</script>
+<style>
+  .myname {
+    width: 90vw;
+  }
+  .myname-title{
+    font-size: 100%;
+  }
+</style>
